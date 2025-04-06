0b58acae25aa0a55b782c4868f64bbb5da644b90
MikolajGl <mikolaj.grygiel2000@gmail.com>
2025-04-06 03:51:26 +0200

    1.7

--- public/products.js
+++ public/products.js
@@ -1,5 +1,23 @@
 const token = localStorage.getItem('token');
 
+if (!token) {
+  window.location.href = 'login.html';
+}
+
+let payload;
+try {
+  payload = JSON.parse(atob(token.split('.')[1]));
+} catch (err) {
+  alert('Błędny token. Zaloguj się ponownie.');
+  localStorage.removeItem('token');
+  window.location.href = 'login.html';
+}
+
+if (payload.role !== 'admin') {
+  alert('Brak dostępu: tylko dla admina');
+  window.location.href = 'test1.html';
+}
+
   document.getElementById('productForm').addEventListener('submit', async (e) => {
     e.preventDefault();
 
